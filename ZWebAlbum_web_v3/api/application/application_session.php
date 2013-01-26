05d1ec30e269ecb2548e56e5400d9ae9f2ed2e0f
CyberZHG <443442708@qq.com>
2013-01-26 08:52:06 +0800

    修正登录接口。
    Signed-off-by: CyberZHG <443442708@qq.com>

--- ZWebAlbum_web_v3/api/application/application_session.php
+++ ZWebAlbum_web_v3/api/application/application_session.php
@@ -11,59 +11,40 @@
             include_once $_SERVER['DOCUMENT_ROOT'].'/api/account/account_user.php';
             /**
              * The page is used for the login of user. 
-             * The page returns nothing when there is no query string.
-             * The page echos a new session id when query with "apikey" only.
+             * The page returns nothing without the query string "apikey".
+             * The page echos a new session id when query with "apikey" only and the user is already login.
              * The page echos "SUCCESSED" when the user is already login, and query with "apikey" and "session".
-             * The page echos "SUCCESSED" when query with "apikey", "session", "name", "password".
+             * The page echos "SUCCESSED" when query with "apikey", "name", "password".
              */
             if(isset($_GET["apikey"]))
             {
                 if(isset($_GET["session"]))
                 {
-                    $session = $_GET["session"];
-                    session_start($session);
-                    if(isset($_GET["name"]) && isset($_SET["password"]))
+                    $session_id = $_GET["session"];
+                    session_start($session_id);
+                    if(isset($_GET["User_ID"]))
+                    {
+                        echo "SUCCESSED";
+                    }
+                }
+                else
+                {
+                    if(isset($_GET["name"]) && isset($_GET["password"]))
                     {
                         $name = $_GET["name"];
                         $password = $_GET["password"];
                         $result = Account_User::login($name, $password, $session);
                         if($result == LOGIN_SUCCESS)
                         {
-                            echo "SUCCESSED";
+                            echo "SUCCESSED_" + session_id();
                         }
                         else
                         {
-                            switch($result)
-                            {
-                                case LOGIN_ERROR_NAME:
-                                    echo "用户名格式不合法";
-                                    break;
-                                case LOGIN_ERROR_PASSWORD:
-                                    echo "密码格式不合法";
-                                    break;
-                                case LOGIN_ERROR_EXIST:
-                                    echo "用户名不存在";
-                                    break;
-                                case LOGIN_ERROR_WRONG:
-                                    echo "用户名或密码错误";
-                                    break;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if($_SESSION["User_ID"])
-                        {
-                            echo "SUCCESSED";
+                            echo Account_User::getLoginString($result);
                         }
                     }
                 }
             }
-            else
-            {
-                session_start();
-                echo session_id();
-            }    
         ?>
     </body>
 </html>
