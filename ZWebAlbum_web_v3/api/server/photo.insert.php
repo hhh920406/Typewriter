f77eff41f25f9104d9182269d3ae61cda664a952
CyberZHG <443442708@qq.com>
2013-01-30 19:17:28 +0800

    添加上传文件的接收。
    Signed-off-by: CyberZHG <443442708@qq.com>

--- ZWebAlbum_web_v3/api/server/photo.insert.php
+++ ZWebAlbum_web_v3/api/server/photo.insert.php
@@ -1,15 +1,38 @@
 <?php
+
+function getExtension($name)
+{
+    for($i = count($name) - 1; $i >= 0; -- $i)
+    {
+        if($name[$i] == ".")
+        {
+            return substr($name, $i, count($name) - $i);
+        }
+    }
+    return "";
+}
+
 include_once $_SERVER["DOCUMENT_ROOT"] . "/api/application/application_token.php";
 include_once $_SERVER["DOCUMENT_ROOT"] . "/api/album/album_photo.php";
 $albumID = $_POST["album_id"];
-$description = $_POST["photo_description"];
-$path = $_POST["photo_path"];
-// TODO Upload the photo.
-$photoInfo = array(
-    "AlbumID" => $albumID,
-    "Description" => $description,
-    "PhotoPath" => $path
-);
 
-echo json_decode(array("album_photo" => Album_Photo::insert($photoInfo)));
+if($_FILES["upload"]["error"] == UPLOAD_ERR_OK)
+{
+    $description = $_FILES["upload"]["name"];
+    $extension = getExtension($_FILES["upload"]["name"]);
+    $tempPath = $_SERVER["DOCUMENT_ROOT"] . "/temp/" . $_FILES["upload"]["tmp_name"];
+    $path = $_SERVER["DOCUMENT_ROOT"] . "/storage/" . $albumID . "/" . substr(md5(uniqid(mt_rand(), true)), 0, 32) . $extension;
+    copy($tempPath, $path);
+    $photoInfo = array(
+        "AlbumID" => $albumID,
+        "Description" => $description,
+        "PhotoPath" => $path
+    );
+    echo json_encode(array("photo_id" => Album_Photo::insert($photoInfo)));
+}
+else
+{
+    echo json_encode(array("photo_id" => 0));
+}
+
 ?>
