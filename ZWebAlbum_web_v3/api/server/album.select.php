7c377ebaae216b5c4b62a79040bd814c7cd3b60c
CyberZHG <443442708@qq.com>
2013-03-04 22:11:53 +0800

    接口返回图片路径改为完整URL地址。
    Signed-off-by: CyberZHG <443442708@qq.com>

--- ZWebAlbum_web_v3/api/server/album.select.php
+++ ZWebAlbum_web_v3/api/server/album.select.php
@@ -1,21 +1,28 @@
 <?php
 include_once $_SERVER["DOCUMENT_ROOT"] . "/api/application/application_token.php";
 include_once $_SERVER["DOCUMENT_ROOT"] . "/api/album/album_album.php";
+
+$albums = "";
 if(isset($_POST["album_id"]))
 {
     $albumID = $_POST["album_id"];
-    echo json_encode(Album_Album::select($albumID));
+    $albums = Album_Album::select($albumID);
 }
 else
 {
     $userID = Application_Token::getUserID($token);
     if(isset($_POST["limit_start"]) && isset($_POST["limit_number"]))
     {
-        echo json_encode(Album_Album::getAlbums($userID, $_POST["limit_start"], $_POST["limit_number"]));
+        $albums = Album_Album::getAlbums($userID, $_POST["limit_start"], $_POST["limit_number"]);
     } 
     else
     {    
-        echo json_encode(Album_Album::getAlbums($userID));
+        $albums = Album_Album::getAlbums($userID);
     }
 }
+for($i = 0; $i < count($albums); ++ $i)
+{
+    $albums[$i]["PhotoPath"] = "http://" . $_SERVER["HTTP_HOST"] . $albums[$i]["PhotoPath"];
+}
+echo json_encode($albums);
 ?>
