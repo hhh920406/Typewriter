21183bbb044b7118b746ff068e7655a92d63c2cc
CyberZHG <443442708@qq.com>
2013-03-05 22:21:41 +0800

    添加对时间的照片分类。
    Signed-off-by: CyberZHG <443442708@qq.com>

--- ZWebAlbum_web_v3/app/clarify/exec/Clarify_Color/Program.cs
+++ ZWebAlbum_web_v3/app/clarify/exec/Clarify_Color/Program.cs
@@ -13,60 +13,67 @@ namespace Clarify_Color
             if (args.Length > 0)
             {
                 string path = args[0];
-                Bitmap bitmap = new Bitmap(path);
-                int[] hueScope = new int[7];
-                for (int i = 0; i < 7; ++i)
+                try
                 {
-                    hueScope[i] = 0;
-                }
-                for (int x = 0; x < bitmap.Width; ++x)
-                {
-                    for (int y = 0; y < bitmap.Height; ++y)
+                    Bitmap bitmap = new Bitmap(path);
+                    int[] hueScope = new int[7];
+                    for (int i = 0; i < 7; ++i)
                     {
-                        int hue = (int)bitmap.GetPixel(x, y).GetHue();
-                        if (hue >= 290 || hue <= 20)
+                        hueScope[i] = 0;
+                    }
+                    for (int x = 0; x < bitmap.Width; ++x)
+                    {
+                        for (int y = 0; y < bitmap.Height; ++y)
                         {
-                            ++hueScope[0];
-                        }
-                        else if (hue >= 21 && hue <= 40)
-                        {
-                            ++hueScope[1];
-                        }
-                        else if (hue >= 41 && hue <= 75)
-                        {
-                            ++hueScope[2];
-                        }
-                        else if (hue >= 76 && hue <= 155)
-                        {
-                            ++hueScope[3];
-                        }
-                        else if (hue >= 156 && hue <= 190)
-                        {
-                            ++hueScope[4];
-                        }
-                        else if (hue >= 191 && hue <= 270)
-                        {
-                            ++hueScope[5];
-                        }
-                        else
-                        {
-                            ++hueScope[6];
+                            int hue = (int)bitmap.GetPixel(x, y).GetHue();
+                            if (hue >= 290 || hue <= 20)
+                            {
+                                ++hueScope[0];
+                            }
+                            else if (hue >= 21 && hue <= 40)
+                            {
+                                ++hueScope[1];
+                            }
+                            else if (hue >= 41 && hue <= 75)
+                            {
+                                ++hueScope[2];
+                            }
+                            else if (hue >= 76 && hue <= 155)
+                            {
+                                ++hueScope[3];
+                            }
+                            else if (hue >= 156 && hue <= 190)
+                            {
+                                ++hueScope[4];
+                            }
+                            else if (hue >= 191 && hue <= 270)
+                            {
+                                ++hueScope[5];
+                            }
+                            else
+                            {
+                                ++hueScope[6];
+                            }
                         }
                     }
-                }
-                int maxHue = 0;
-                for (int i = 1; i < 7; ++i)
-                {
-                    if (hueScope[i] > hueScope[maxHue])
+                    int maxHue = 0;
+                    for (int i = 1; i < 7; ++i)
                     {
-                        maxHue = i;
+                        if (hueScope[i] > hueScope[maxHue])
+                        {
+                            maxHue = i;
+                        }
                     }
+                    Console.Write(maxHue);
                 }
-                Console.Write(maxHue);
+                catch
+                {
+                    Console.WriteLine(-1);
+                }
             }
             else
             {
-                Console.Write(-1);
+                Console.WriteLine(-1);
             }
         }
     }
---
+++ ZWebAlbum_web_v3/app/clarify/exec/Clarify_Time/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Clarify_Time
+{
+    class Program
+    {
+        static bool isNumber(char ch)
+        {
+            return ch >= '0' && ch <= '9';
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                try
+                {
+                    string path = args[0];
+                    Bitmap bitmap = new Bitmap(path);
+                    string value = Encoding.Default.GetString(bitmap.GetPropertyItem(0x0132).Value);
+                    bool flag = true;
+                    if (value.Length < 7)
+                    {
+                        flag = false;
+                    }
+                    else if (!isNumber(value[0]) || !isNumber(value[1]) || !isNumber(value[2]) || !isNumber(value[3]))
+                    {
+                        flag = false;
+                    }
+                    else if (isNumber(value[4]))
+                    {
+                        flag = false;
+                    }
+                    else if (!isNumber(value[5]) || !isNumber(value[6]))
+                    {
+                        flag = false;
+                    }
+                    if (flag)
+                    {
+                        value = value.Substring(0, 4) + "-" + value.Substring(5, 2);
+                        Console.WriteLine(value);
+                    }
+                    else
+                    {
+                        Console.WriteLine(-1);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine(-1);
+                }
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+        }
+    }
+}
