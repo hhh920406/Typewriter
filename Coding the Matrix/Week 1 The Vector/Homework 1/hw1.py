4032aef46feff692689d38d6ab82129cabf996ee
CyberZHG <443442708@qq.com>
2013-07-14 16:20:20 +0800

    Code the Matrix => Week 1 => Homework 1
    Signed-off-by: CyberZHG <443442708@qq.com>

---
+++ Coding the Matrix/Week 1 The Vector/Homework 1/hw1.py
@@ -0,0 +1,43 @@
+from GF2 import one
+
+## Problem 1
+p1_u = [ 0, 4]
+p1_v = [-1, 3]
+p1_v_plus_u = [-1, 7]
+p1_v_minus_u = [-1, -1]
+p1_three_v_minus_two_u = [-3, 1]
+
+## Problem 2
+p2_u = [-1,  1, 1]
+p2_v = [ 2, -1, 5]
+p2_v_plus_u = [1, 0, 6]
+p2_v_minus_u = [3, -2, 4]
+p2_two_v_minus_u = [5, -3, 9]
+p2_v_plus_two_u = [0, 1, 7]
+
+## Problem 3
+p3_vector_sum_1 = [one, 0, 0]
+p3_vector_sum_2 = [0, one, one]
+
+## Problem 4
+u_0010010 = {'c', 'd', 'e'}
+u_0100010 = {'b', 'c', 'd', 'e'}
+
+## Problem 5
+v_0010010 = {'c', 'd'}
+v_0100010 = set()
+
+## Problem 6
+uv_a = 5
+uv_b = 6
+uv_c = 16
+uv_d = -1
+
+## Problem 7
+x_gf2 = [one, 0, 0, 0]
+
+## Problem 8
+v1 = [2, 3, -4, 1]
+v2 = [1, -5, 2, 0]
+v3 = [4, 1, -1, -1]
+
