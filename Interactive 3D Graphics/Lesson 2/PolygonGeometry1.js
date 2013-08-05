fa84d28ac630ec18a12aae78e486f34230721d4d
CyberZHG <CyberZHG@gmail.com>
2013-08-05 09:25:59 +0800

    Interactive 3D Graphics => Lesson 2 => PolygonGeometry
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

---
+++ Interactive 3D Graphics/Lesson 2/PolygonGeometry1.js
@@ -0,0 +1,14 @@
+function PolygonGeometry(sides) {
+    var geo = new THREE.Geometry();
+    for (var pt = 0; pt < sides; ++pt) {
+        var angle = (Math.PI / 2) + (pt / sides) * 2 * Math.PI;
+        var x = Math.cos(angle);
+        var y = Math.sin(angle);
+        geo.vertices.push(new THREE.Vector3(x, y, 0));
+    }
+    for (pt = 2; pt < sides; ++pt) {
+        geo.faces.push(new THREE.Face3(0, pt - 1, pt));
+    }
+    return geo;
+}
+
