7489096f4f8ef560c022de967eef0dce380d9786
CyberZHG <CyberZHG@gmail.com>
2013-08-06 21:28:52 +0800

    Interactive 3D Graphics => Lesson 8 => Pick a Letter
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

---
+++ Interactive 3D Graphics/Lesson 8/Pick a Letter.js
@@ -0,0 +1,5 @@
+var uvs = [];
+uvs.push( new THREE.Vector2( 0.75, 0.25 ) );
+uvs.push( new THREE.Vector2( 1.0, 0.25 ) );
+uvs.push( new THREE.Vector2( 1.0, 0.5 ) );
+uvs.push( new THREE.Vector2( 0.75, 0.5 ) );
