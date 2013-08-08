91472503bbb7408e722b5337690a0b0b863599c7
CyberZHG <CyberZHG@gmail.com>
2013-08-08 22:34:59 +0800

    HTML5 Game Development => Classes
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

---
+++ HTML5 Game Development/Classes.js
@@ -0,0 +1,6 @@
+Weapon = Class.extend({});
+MachineGun = Weapon.extend({});
+ChainGun = Weapon.extend({});
+Entity = Class.extend({});
+Teleporter = Entity.extend({});
+EnergyCanister = Entity.extend({});
