88be922a81c04c8ea6406ea8030f889f1c89eea0
CyberZHG <CyberZHG@gmail.com>
2013-08-09 21:43:27 +0800

    HTML5 Game Development => Physics => The World
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

---
+++ HTML5 Game Development/Physics/The World.js
@@ -0,0 +1,20 @@
+Vec2 = Box2D.Common.Math.b2Vec2;
+BodyDef = Box2D.Dynamics.b2BodyDef;
+Body = Box2D.Dynamics.b2Body;
+FixtureDef = Box2D.Dynamics.b2FixtureDef;
+Fixture = Box2D.Dynamics.b2Fixture;
+World = Box2D.Dynamics.b2World;
+MassData = Box2D.Collision.Shapes.b2MassData;
+PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
+CircleShape = Box2D.Collision.Shapes.b2CircleShape;
+DebugDraw = Box2D.Dynamics.b2DebugDraw;
+RevoluteJointDef = Box2D.Dynamics.Joints.b2RevoluteJointDef;
+
+PhysicsEngineClass = Class.extend({
+    world: null,
+    create: function () {
+        gPhysicsEngine.world = new World(new Vec2(), false);
+    }
+});
+
+var gPhysicsEngine = new PhysicsEngineClass();
