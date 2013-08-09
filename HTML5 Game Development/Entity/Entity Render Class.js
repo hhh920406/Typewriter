b44d751ac36c81bf309fb3e41e7bdfe3d5704a0a
CyberZHG <CyberZHG@gmail.com>
2013-08-09 21:41:50 +0800

    HTML5 Game Development => Entity => Entity Render Class
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

---
+++ HTML5 Game Development/Entity/Entity Render Class.js
@@ -0,0 +1,8 @@
+EntityClass = Class.extend({
+    pos : {x:0,y:0},
+    size : {x:0,y:0},
+    last : {x:0,y:0},
+    currSpriteName : '',
+    update : function() { },
+    draw : function() { }
+});
