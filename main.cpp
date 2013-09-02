253bbc6f64183c45e23d281a2f6dbfe014e65b35
CyberZHG <CyberZHG@gmail.com>
2013-09-02 14:39:36 +0800

    删除测试文件。
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include "Framework.h"
-#include "SceneIncrease.h"
+#include "TestScene.h"
 #include "SceneController.h"
 
 int main()
 {
     Framework *framework = Framework::getInstance();
     framework->init("Framework", 800, 600, false);
-    SceneIncrease *scene = new SceneIncrease();
+    TestScene *scene = new TestScene();
     framework->getInstance()->sceneController()->bindScene(0, scene);
     framework->getInstance()->sceneController()->setFirstScene(0);
     framework->messageLoop();
