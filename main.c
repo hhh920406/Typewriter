ae8a0d0b02527a3e59f45f5c9db294d23c031432
cyberzhg <cyberzhg@gmail.com>
2014-01-06 12:27:32 +0800

    Stars.
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

--- main.c
+++ main.c
@@ -8,13 +8,14 @@
 
 #define D3DFVF D3DFVF_XYZ | D3DFVF_DIFFUSE | D3DFVF_TEX1
 
-const char* WINDOW_CLASS_NAME = "Universe";
-const char* WINDOW_CAPTION = "Universe";
+const char* WINDOW_CLASS_NAME = "Stars";
+const char* WINDOW_CAPTION = "Stars";
 const int WINDOW_POSITION_X = 50;
 const int WINDOW_POSITION_Y = 50;
-#define WINDOW_SIZE_WIDTH 800
-#define WINDOW_SIZE_HEIGHT 800
+#define WINDOW_SIZE_WIDTH 1024
+#define WINDOW_SIZE_HEIGHT 768
 const int RECT_LENGTH = 5;
+#define MAX_STAR_NUM 100
 
 LPDIRECT3D9 g_pD3D = NULL;
 LPDIRECT3DDEVICE9 g_pd3dDevice = NULL;
@@ -51,22 +52,6 @@ LPDIRECT3DVERTEXBUFFER9 createRect(int r, int g, int b) {
     return rect;
 }
 
-LPDIRECT3DVERTEXBUFFER9 createDust(int r, int g, int b) {
-    void *ptr;
-    struct stD3DVertex data[] = {
-        {0.5f, -0.5f, 0.5f, D3DCOLOR_XRGB(r, g, b), 1.0f, 0.0f},
-        {0.5f, 0.5f, 0.5f, D3DCOLOR_XRGB(r, g, b), 1.0f, 1.0f},
-        {-0.5f, -0.5f, 0.5f, D3DCOLOR_XRGB(r, g, b), 0.0f, 0.0f},
-        {-0.5f, 0.5f, 0.5f, D3DCOLOR_XRGB(r, g, b), 0.0f, 1.0f},
-    };
-    LPDIRECT3DVERTEXBUFFER9 rect;
-    IDirect3DDevice9_CreateVertexBuffer(g_pd3dDevice, sizeof(data), 0, D3DFVF, D3DPOOL_DEFAULT, &rect, NULL);
-    IDirect3DVertexBuffer9_Lock(rect, 0, sizeof(data), (void**)&ptr, 0);
-    memcpy(ptr, data, sizeof(data));
-    IDirect3DVertexBuffer9_Unlock(rect);
-    return rect;
-}
-
 struct Position {
     double x, y;
 };
@@ -83,19 +68,10 @@ double dist(struct Position a, struct Position b) {
     return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
 }
 
-LPDIRECT3DVERTEXBUFFER9 solar;
-LPDIRECT3DVERTEXBUFFER9 earth;
-LPDIRECT3DVERTEXBUFFER9 moon;
-LPDIRECT3DVERTEXBUFFER9 dust[4];
+LPDIRECT3DVERTEXBUFFER9 starBuffer;
 
 void initPixel() {
-    solar = createRect(190, 86, 25);
-    earth = createRect(20, 53, 230);
-    moon = createRect(220, 255, 230);
-    dust[0] = createDust(150, 150, 150);
-    dust[1] = createDust(150, 150, 150);
-    dust[2] = createDust(150, 150, 150);
-    dust[3] = createDust(150, 150, 150);
+    starBuffer = createRect(249, 249, 249);
 }
 
 void initD3D(HWND hWnd) {
@@ -120,33 +96,19 @@ void initD3D(HWND hWnd) {
 
 int lastTime;
 
-double earthAngle;
-double moonAngle;
+int starNum;
+struct Position starPos[MAX_STAR_NUM];
+struct Position starSpeed[MAX_STAR_NUM];
+double mass[MAX_STAR_NUM];
+double radius[MAX_STAR_NUM];
 
-struct Position solarPos = {WINDOW_SIZE_WIDTH >> 1, WINDOW_SIZE_HEIGHT >> 1};
-struct Position earthPos;
-struct Position moonPos;
-
-#define MAX_DUST_NUM 10000
-int dustNum;
-struct Position dustPos[MAX_DUST_NUM + 10];
-struct Position dustSpeed[MAX_DUST_NUM + 10];
-int dustColor[MAX_DUST_NUM + 10];
-
-double solarMass = 50;
-double earthMass = 10;
-double moonMass = 1;
-
-int escapeNum;
-int solarNum;
-int earthNum;
-int moonNum;
-
-double dustAngle;
-int dustDir;
-
-double dustSpeedInit;
-int dustSpeedDir;
+void deleteStar(int pos) {
+    --starNum;
+    starPos[pos] = starPos[starNum];
+    starSpeed[pos] = starSpeed[starNum];
+    mass[pos] = mass[starNum];
+    radius[pos] = radius[starNum];
+}
 
 void render() {
     int i, j;
@@ -159,165 +121,79 @@ void render() {
     D3DXMATRIX world;
     D3DXMATRIX translation;
     D3DXMATRIX scaling;
-    double solarRadius;
-    double earthRadius;
-    double moonRadius;
     int currentTime = timeGetTime();
     if ((currentTime - lastTime) * 60 < 1000) {
-        //return;
+        return;
     }
     IDirect3DDevice9_Clear(g_pd3dDevice, 0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
     IDirect3DDevice9_BeginScene(g_pd3dDevice);
 
-    earthPos.x = solarPos.x + 250.0 * cos(earthAngle);
-    earthPos.y = solarPos.y + 250.0 * sin(earthAngle);
-    earthAngle -= sqrt(sqrt(earthMass)) / 300.0f;
-
-    moonPos.x = earthPos.x + 50.0 * cos(moonAngle);
-    moonPos.y = earthPos.y + 50.0 * sin(moonAngle);
-    moonAngle += sqrt(sqrt(moonMass)) / 55.0f;
-
-    solarRadius = log(solarMass) / 2.0 * RECT_LENGTH;
-    earthRadius = log(earthMass) / 2.0 * RECT_LENGTH;
-    moonRadius = log(moonMass) / 2.0 * RECT_LENGTH;
-
-    dustAngle = atan2(moonPos.y, moonPos.x);
-    while (dustNum < MAX_DUST_NUM) {
-        if (rand() > 32000) {
-            break;
-        }
-        dustPos[dustNum].x = 0;
-        dustPos[dustNum].y = 0;
-        dustSpeed[dustNum].x = (rand() % 100) / 100.0;
-        dustSpeed[dustNum].y = 1.0;
-        dustColor[dustNum] = 0;
-        ++dustNum;
-        dustPos[dustNum].x = 0;
-        dustPos[dustNum].y = 0;
-        dustSpeed[dustNum].x = (rand() % 100) / 100.0;
-        dustSpeed[dustNum].y = 1.0;
-        dustColor[dustNum] = 0;
-        ++dustNum;
-        dustPos[dustNum].x = WINDOW_SIZE_WIDTH;
-        dustPos[dustNum].y = 0;
-        dustSpeed[dustNum].x = -(rand() % 100) / 100.0;
-        dustSpeed[dustNum].y = 1.0;
-        dustColor[dustNum] = 0;
-        ++dustNum;
-        dustPos[dustNum].x = 0;
-        dustPos[dustNum].y = WINDOW_SIZE_HEIGHT;
-        dustSpeed[dustNum].x = (rand() % 100) / 100.0;
-        dustSpeed[dustNum].y = -1.0;
-        dustColor[dustNum] = 0;
-        ++dustNum;
-        dustPos[dustNum].x = WINDOW_SIZE_WIDTH;
-        dustPos[dustNum].y = WINDOW_SIZE_HEIGHT;
-        dustSpeed[dustNum].x = -(rand() % 100) / 100.0;
-        dustSpeed[dustNum].y = -1.0;
-        dustColor[dustNum] = 0;
-        ++dustNum;
+    for (i = 0; i < starNum; ++i) {
+        radius[i] = sqrt(mass[i]) * RECT_LENGTH;
     }
 
-    for (i = dustNum - 1; i >= 0; --i) {
-        if (dustPos[i].x < -0 || dustPos[i].x > 0 + WINDOW_SIZE_WIDTH) {
-            --dustNum;
-            dustPos[i] = dustPos[dustNum];
-            dustSpeed[i] = dustSpeed[dustNum];
-            dustColor[i] = dustColor[dustNum];
+    for (i = starNum - 1; i >= 0; --i) {
+        for (j = starNum - 1; j > i; --j) {
+            if (dist(starPos[i], starPos[j]) < 10.0 + (radius[i] - radius[j]) * (radius[i] - radius[j])) {
+                if (mass[i] > mass[j]) {
+                    mass[i] += mass[j];
+                    starSpeed[i].x = (starSpeed[i].x * mass[i] + starSpeed[j].x * mass[j]) / (mass[i] + mass[j]);
+                    starSpeed[i].y = (starSpeed[i].y * mass[i] + starSpeed[j].y * mass[j]) / (mass[i] + mass[j]);
+                    deleteStar(j);
+                    break;
+                } else {
+                    mass[j] += mass[i];
+                    starSpeed[j].x = (starSpeed[i].x * mass[i] + starSpeed[j].x * mass[j]) / (mass[i] + mass[j]);
+                    starSpeed[j].y = (starSpeed[i].y * mass[i] + starSpeed[j].y * mass[j]) / (mass[i] + mass[j]);
+                    deleteStar(i);
+                    --i;
+                    break;
+                }
+            }
         }
-        if (dustPos[i].y < -0 || dustPos[i].y > 0 + WINDOW_SIZE_HEIGHT) {
-            --dustNum;
-            dustPos[i] = dustPos[dustNum];
-            dustSpeed[i] = dustSpeed[dustNum];
-            dustColor[i] = dustColor[dustNum];
-        }
-        if (dist(dustPos[i], solarPos) < solarRadius * solarRadius) {
-            solarMass += 1.0;
-            --dustNum;
-            dustPos[i] = dustPos[dustNum];
-            dustSpeed[i] = dustSpeed[dustNum];
-            dustColor[i] = dustColor[dustNum];
-            ++solarNum;
-            continue;
-        }
-        if (dist(dustPos[i], earthPos) < earthRadius * earthRadius) {
-            earthMass += 0.8;
-            --dustNum;
-            dustPos[i] = dustPos[dustNum];
-            dustSpeed[i] = dustSpeed[dustNum];
-            dustColor[i] = dustColor[dustNum];
-            ++earthNum;
-            continue;
-        }
-        if (dist(dustPos[i], moonPos) < moonRadius * moonRadius) {
-            moonMass += 0.7;
-            --dustNum;
-            dustPos[i] = dustPos[dustNum];
-            dustSpeed[i] = dustSpeed[dustNum];
-            dustColor[i] = dustColor[dustNum];
-            ++moonNum;
-            continue;
-        }
-        forceX = 0.0;
-        forceY = 0.0;
+    }
 
-        angle = atan2(solarPos.y - dustPos[i].y, solarPos.x - dustPos[i].x);
-        dist2 = dist(solarPos, dustPos[i]) + 100;
-        force = solarMass / dist2;
-        forceX -= force * cos(angle);
-        forceY -= force * sin(angle);
+    for (i = 0; i < starNum; ++i) {
+        forceX = forceY = 0.0;
+        for (j = 0; j < starNum; ++j) {
+            if (i != j) {
+                dist2 = dist(starPos[i], starPos[j]);
+                angle = atan2(starPos[j].y - starPos[i].y, starPos[j].x - starPos[i].x);
+                force = mass[j] / (dist2 + 0.01);
+                forceX += force * cos(angle);
+                forceY += force * sin(angle);
+            }
+        }
+        starSpeed[i].x += forceX / mass[i];
+        starSpeed[i].y += forceY / mass[i];
+        starPos[i].x += starSpeed[i].x;
+        starPos[i].y += starSpeed[i].y;
+    }
 
-        angle = atan2(earthPos.y - dustPos[i].y, earthPos.x - dustPos[i].x);
-        dist2 = dist(earthPos, dustPos[i]);
-        force = earthMass / dist2;
-        forceX += force * cos(angle);
-        forceY += force * sin(angle);
-
-        angle = atan2(moonPos.y - dustPos[i].y, moonPos.x - dustPos[i].x);
-        dist2 = dist(moonPos, dustPos[i]);
-        force = moonMass / dist2;
-        forceX -= force * cos(angle);
-        forceY -= force * sin(angle);
-
-        dustSpeed[i].x += forceX;
-        dustSpeed[i].y += forceY;
-        dustPos[i].x += dustSpeed[i].x;
-        dustPos[i].y += dustSpeed[i].y;
-        D3DXMatrixTranslation(&translation, shiftX(dustPos[i]), shiftY(dustPos[i]), 1.0f);
-        IDirect3DDevice9_SetTransform(g_pd3dDevice, D3DTS_WORLD, &translation);
-        IDirect3DDevice9_SetStreamSource(g_pd3dDevice, 0, dust[dustColor[i]], 0, sizeof(struct stD3DVertex));
+    for (i = 0; i < starNum; ++i) {
+        D3DXMatrixTranslation(&translation, shiftX(starPos[i]), shiftY(starPos[i]), 1.0f);
+        D3DXMatrixScaling(&scaling, sqrt(mass[i]), sqrt(mass[i]), 1.0f);
+        D3DXMatrixMultiply(&world, &scaling, &translation);
+        IDirect3DDevice9_SetTransform(g_pd3dDevice, D3DTS_WORLD, &world);
+        IDirect3DDevice9_SetStreamSource(g_pd3dDevice, 0, starBuffer, 0, sizeof(struct stD3DVertex));
         IDirect3DDevice9_SetFVF(g_pd3dDevice, D3DFVF);
-        IDirect3DDevice9_DrawPrimitive(g_pd3dDevice, D3DPT_TRIANGLESTRIP, 0, 2);
+        IDirect3DDevice9_DrawPrimitive(g_pd3dDevice, D3DPT_TRIANGLEFAN, 0, 36);
     }
-
-    D3DXMatrixTranslation(&translation, shiftX(moonPos), shiftY(moonPos), 1.0f);
-    D3DXMatrixScaling(&scaling, log(moonMass), log(moonMass), 1.0f);
-    D3DXMatrixMultiply(&world, &scaling, &translation);
-    IDirect3DDevice9_SetTransform(g_pd3dDevice, D3DTS_WORLD, &world);
-    IDirect3DDevice9_SetStreamSource(g_pd3dDevice, 0, moon, 0, sizeof(struct stD3DVertex));
-    IDirect3DDevice9_SetFVF(g_pd3dDevice, D3DFVF);
-    IDirect3DDevice9_DrawPrimitive(g_pd3dDevice, D3DPT_TRIANGLEFAN, 0, 36);
-
-    D3DXMatrixTranslation(&translation, shiftX(earthPos), shiftY(earthPos), 1.0f);
-    D3DXMatrixScaling(&scaling, log(earthMass), log(earthMass), 1.0f);
-    D3DXMatrixMultiply(&world, &scaling, &translation);
-    IDirect3DDevice9_SetTransform(g_pd3dDevice, D3DTS_WORLD, &world);
-    IDirect3DDevice9_SetStreamSource(g_pd3dDevice, 0, earth, 0, sizeof(struct stD3DVertex));
-    IDirect3DDevice9_SetFVF(g_pd3dDevice, D3DFVF);
-    IDirect3DDevice9_DrawPrimitive(g_pd3dDevice, D3DPT_TRIANGLEFAN, 0, 36);
-
-    D3DXMatrixTranslation(&translation, shiftX(solarPos), shiftY(solarPos), 1.0f);
-    D3DXMatrixScaling(&scaling, log(solarMass), log(solarMass), 1.0f);
-    D3DXMatrixMultiply(&world, &scaling, &translation);
-    IDirect3DDevice9_SetTransform(g_pd3dDevice, D3DTS_WORLD, &world);
-    IDirect3DDevice9_SetStreamSource(g_pd3dDevice, 0, solar, 0, sizeof(struct stD3DVertex));
-    IDirect3DDevice9_SetFVF(g_pd3dDevice, D3DFVF);
-    IDirect3DDevice9_DrawPrimitive(g_pd3dDevice, D3DPT_TRIANGLEFAN, 0, 36);
 
     IDirect3DDevice9_EndScene(g_pd3dDevice);
     IDirect3DDevice9_Present(g_pd3dDevice, NULL, NULL, NULL, NULL);
+}
 
-    printf("%.15lf %.15lf\n", dustPos[1].x - dustPos[0].x, dustPos[1].y - dustPos[0].y);
+void restart() {
+    int i;
+    for (i = 0; i < MAX_STAR_NUM; ++i) {
+        starPos[i].x = rand() % WINDOW_SIZE_WIDTH;
+        starPos[i].y = rand() % WINDOW_SIZE_HEIGHT;
+        starSpeed[i].x = 0.0;
+        starSpeed[i].y = 0.0;
+        mass[i] = 3.0;
+    }
+    starNum = MAX_STAR_NUM;
 }
 
 LRESULT WINAPI messageProcess(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
@@ -325,6 +201,9 @@ LRESULT WINAPI messageProcess(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     case WM_PAINT:
         render();
         ValidateRect(hWnd, NULL);
+        break;
+    case WM_KEYUP:
+        restart();
         break;
     case WM_DESTROY:
         PostQuitMessage(0);
