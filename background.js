e9ce83d68e3bef59a30b53be6a32c172eebfcd37
cyberzhg <cyberzhg@gmail.com>
2014-01-11 16:13:04 +0800

    可以通过弹出窗口设置点赞ID。
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

--- background.js
+++ background.js
@@ -1,5 +1,18 @@
 chrome.webNavigation.onDOMContentLoaded.addListener(function(details) {
   if (details.url.indexOf("renren.com") >= 0) {
-	chrome.tabs.executeScript(details.tabId, {file: "zan.js"});
+    chrome.storage.sync.get('ids', function(items) {
+	  code = "zan_array = [";
+      if (items.ids) {
+        for (var i = 0; i < items.ids.length; ++i) {
+		  if (i > 0) {
+		    code += ",";
+		  }
+		  code += items.ids[i];
+		}
+	  }
+	  code += "];";
+	  chrome.tabs.executeScript(details.tabId, {code: code});
+	  chrome.tabs.executeScript(details.tabId, {file: "zan.js"});
+    });
   }
 });
--- zan.js
+++ zan.js
@@ -3,19 +3,20 @@ if (!document.getElementById("zan_global")) {
   div.id = "zan_global";
   document.body.appendChild(div);
 
-  console.log('Inside');
   window.setInterval(function() {
     var links = document.getElementsByClassName("ilike_icon");
-    console.log("Finding...", links.length);
     for (var i = 0; i < links.length; ++i) {
       var strs = links[i].getAttribute('onclick').split(",");
       if (strs.length >= 5) {
 	    if (links[i].innerText.indexOf("取消") < 0) {
-          if (strs[3].indexOf("283858273") >= 0 || strs[3].indexOf("287105729") >= 0) {
-		    console.log(i, links[i].innerText);
-	        links[i].click();
-		    return;
-	      }
+		  for (var j = 0; j < zan_array.length; ++j) {
+		    if (zan_array[j].length > 5) {
+			  if (strs[3].indexOf(zan_array[j]) >= 0) {
+			    console.log(links[i].innerText);
+				return;
+			  }
+			}
+		  }
 	    }
       }
     }
