491e61da26c8d1406220fb1df060ae4763512355
cyberzhg <cyberzhg@gmail.com>
2014-01-11 16:58:39 +0800

    添加开始与停止按钮。
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

--- popup.js
+++ popup.js
@@ -7,15 +7,40 @@ function saveUserID() {
 }
 
 function loadUserID() {
-  chrome.storage.sync.get('ids', function(items) {
-    if (items.ids) {
-      ids = items.ids;
+  chrome.storage.sync.get('ids', function(item) {
+    if (item.ids) {
+      ids = item.ids;
 	  area_id.value = ids.join("\n");
 	}
+  });
+}
+
+function updateStartButton() {
+  chrome.storage.sync.get('start', function(item) {
+    if (item.start) {
+	  button_start.innerText = "运行中";
+	  button_start.className = "running";
+	} else {
+	  button_start.innerText = "已停止";
+	  button_start.className = "stop";
+	}
+  });
+}
+
+function toggleStart() {
+  chrome.storage.sync.get('start', function(item) {
+    if (item.start) {
+	  chrome.storage.sync.set({'start': false});
+	} else {
+	  chrome.storage.sync.set({'start': true});
+	}
+    updateStartButton();
   });
 }
 
 window.onload = function() {
   loadUserID();
   button_save.onclick = saveUserID;
+  button_start.onclick = toggleStart;
+  updateStartButton();
 }
